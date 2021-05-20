3d7745e7ba04fb1484fa9aceb7482c067c628c09
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2021-05-20 14:23:41 +0900

    Reafctor & fill all features for 'Todo'

--- todo.py
+++ todo.py
@@ -1,43 +1,53 @@
-from typing import TypedDict, Set, Final
+from typing import TypedDict, Set, Optional, Tuple
 from datetime import datetime, date
 
 Todo = TypedDict('Todo', {
     'content': str,
     'start': date,
+    'finish': Optional[date],
     'deadline': date,
     'tags': Set[str],
-    'expectTimeHour': int,
 })
- 
+
 def createTodo(content: str, deadline: date) -> Todo:
     if content == '':
         raise Exception('空の TODO を作成することはできません！')
-    
+
     if deadline < datetime.now():
         raise Exception('過去の日付で TODO を作成することはできません！')
-    
+
     return {
         'content': content,
         'start': datetime.now(),
+        'finish': None,
         'deadline': deadline,
         'tags': set(),
-        'expectTimeHour': 72, # 3日間を標準の期限とする
     }
-    
+
+def finishTodo(todo: Todo) -> Tuple[Todo, int]:
+    todo['finish'] = datetime.now()
+
+    expect_delta = getattr(todo['deadline'] - todo['start'], 'hours', 1)
+    actual_delta = getattr(todo['finish'] - todo['start'], 'hours', 1) # type: ignore[operator]
+
+    ratio = int(actual_delta / expect_delta * 100)
+
+    return (todo, ratio)
+
 def editDeadline(todo: Todo, deadline: date) -> Todo:
     if deadline < datetime.now():
         raise Exception('過去の日付に TODO を編集することはできません！')
-    
+
     todo['deadline'] = deadline
-    
+
     return todo
-    
+
 def addTag(todo: Todo, tag: str) -> Todo:
     if tag == '':
         raise Exception('空のタグを設定することはできません！')
-    
+
     todo['tags'].add(tag)
-    
+
     return todo
 
 def removeTag(todo: Todo, tag: str) -> Todo:
@@ -45,5 +55,5 @@ def removeTag(todo: Todo, tag: str) -> Todo:
         todo['tags'].remove(tag)
     else:
         raise Exception('存在しないタグを削除しようとしました！')
-        
+
     return todo
