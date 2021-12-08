c099a0c67448bbed9663e99ceb2016ce1d4a85d2
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2021-12-08 12:38:38 +0900

    Add cors support

--- index.py
+++ index.py
@@ -10,6 +10,14 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 todolist = TodoList()
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+
+    return response
+
 @app.route('/', methods=['GET'])
 def get_index():
     return render_template('index.html')
